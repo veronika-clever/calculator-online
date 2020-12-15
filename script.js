bb3b4e77644ca83208f9560ce987779e4fc61b90
unknown <veorra2019@gmail.com>
2020-12-16 01:37:26 +0300

    update.md

--- script.js
+++ script.js
@@ -2,134 +2,138 @@ let lastOperand = 0;
 let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
+const aboveWindow = document.getElementById('aboveWindow');
 
 
 document.getElementById('btn_1').addEventListener('click', function () {
+  if(inputWindow.value == '0') inputWindow.value = '';
   inputWindow.value += '1';
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
-    inputWindow.value += '2';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '2';
   })
 
 document.getElementById('btn_3').addEventListener('click', function () {
-    inputWindow.value += '3';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '3';
   })
 
 document.getElementById('btn_4').addEventListener('click', function () {
-    inputWindow.value += '4';
+  if(inputWindow.value == '0') inputWindow.value = ''; 
+  inputWindow.value += '4';
   })
 
 document.getElementById('btn_5').addEventListener('click', function () {
-    inputWindow.value += '5';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '5';
   })
   
 document.getElementById('btn_6').addEventListener('click', function () {
-    inputWindow.value += '6';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '6';
   })  
 
 document.getElementById('btn_7').addEventListener('click', function () {
-    inputWindow.value += '7';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '7';
   }) 
 
 document.getElementById('btn_8').addEventListener('click', function () {
-    inputWindow.value += '8';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '8';
   }) 
 
 document.getElementById('btn_9').addEventListener('click', function () {
-    inputWindow.value += '9';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '9';
   }) 
 
 document.getElementById('btn_null').addEventListener('click', function () {
-    inputWindow.value += '0';
+  if(inputWindow.value == '0') inputWindow.value = '';
+  inputWindow.value += '0';
   }) 
 
-document.getElementById('btn_material').addEventListener('click', function () {
-    inputWindow.value += '.';
-  })
+document.querySelector("#btn_point").addEventListener('click', function () {
+    if (inputWindow.value.includes('.') == false) {
+        inputWindow.value += ".";
+    }
+})
 
 document.getElementById('btn_sum').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'sum';
-    inputWindow.value = '';
-
-    console.log(lastOperand);
-    console.log(operation);
+    inputWindow.value = '0';
   })
 
 document.getElementById('btn_min').addEventListener('click', function () {
-        lastOperand = inputWindow.value;
+        lastOperand = parseFloat(inputWindow.value);
         operation = 'min';
-        inputWindow.value = ''; 
-
-    console.log(lastOperand);
-    console.log(operation);
+        inputWindow.value = '0'; 
   })
 
 document.getElementById('btn_multiplication').addEventListener('click', function () {
-    lastOperand = inputWindow.value;
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'multiplication';
-    inputWindow.value = '';
-
-    console.log(lastOperand);
-    console.log(operation);
+    inputWindow.value = '0';
   })
 
 document.getElementById('btn_sqrt').addEventListener('click', function () {
-    lastOperand = inputWindow.value;
-    operation = 'sqrt';
-    inputWindow.value = '';
-
-    console.log(lastOperand);
-    console.log(operation);
+  var val = parseFloat(inputWindow.value);
+  inputWindow.value = Math.sqrt(val);
   })
 
   document.getElementById('btn_divis').addEventListener('click', function () {
-    lastOperand = inputWindow.value;
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'divis';
-    inputWindow.value = '';
-
-    console.log(lastOperand);
-    console.log(operation);
+    inputWindow.value = '0';
   })
   
 document.getElementById('btn_calc').addEventListener('click', function () {
     if (operation === 'sum'){
-        const result = lastOperand + parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+      const resultTwo = parseFloat(inputWindow.value);
+      const result = lastOperand + resultTwo;
+      inputWindow.value = result;
+      aboveWindow.value += "\n" + lastOperand + " + " + resultTwo + " = " + result;
+      aboveWindow.scrollTop = aboveWindow.clientHeight;
+      lastOperand = resultTwo;
     }
     if (operation === 'min'){
-        const result = lastOperand - parseInt(inputWindow.value) ;
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result  ;
+      const resultTwo = parseFloat(inputWindow.value);
+      const result = lastOperand - resultTwo;
+      inputWindow.value = result;
+      aboveWindow.value += "\n" + lastOperand + " - " + resultTwo + " = " + result;
+      aboveWindow.scrollTop = aboveWindow.clientHeight;
+      lastOperand = resultTwo; 
     }
     if (operation === 'multiplication'){
-        const result = lastOperand * parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+      const resultTwo = parseFloat(inputWindow.value);
+      const result = lastOperand * resultTwo;
+      inputWindow.value = result;
+      aboveWindow.value += "\n" + lastOperand + " * " + resultTwo + " = " + result;
+      aboveWindow.scrollTop = aboveWindow.clientHeight;
+      lastOperand = resultTwo;
     }
     if (operation === 'sqrt'){
-        const result = Math.sqrt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+      lastOperand = parseFloat(inputWindow.value);
+    inputWindow.value = Math.sqrt(lastOperand);
     }
     if (operation === 'divis'){
-        const result = lastOperand / parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+      const resultTwo = parseFloat(inputWindow.value);
+      const result = lastOperand / resultTwo;
+      inputWindow.value = result;
+      aboveWindow.value += "\n" + lastOperand + " / " + resultTwo + " = " + result;
+      aboveWindow.scrollTop = aboveWindow.clientHeight;
+      lastOperand = resultTwo;
     }
   })
-
 
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
-    inputWindow.value = '';
+    inputWindow.value = '0';
 })
+
+
